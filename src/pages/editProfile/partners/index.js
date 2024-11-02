7c21790ef0e669e62a2b7f57daf59653ff0f742c
DanteAguilarB <142114920+DanteAguilarB@users.noreply.github.com>
2024-11-02 11:58:28 -0400

    added ScrollBar component and tests

--- src/pages/editProfile/partners/index.js
+++ src/pages/editProfile/partners/index.js
@@ -1,7 +1,19 @@
+import ScrollBar from "../../../../components/ScrollBar";
+
 export default function EditPartners() {
+  const previousPartners = [
+    { id: 1, name: "Previous Partner 1" },
+    { id: 2, name: "Previous Partner 2" },
+    { id: 3, name: "Previous Partner 3" },
+  ];
+
   return (
     <div>
-      <h1>Edit Partners</h1>
+      <h2>Edit Partners</h2>
+      <ScrollBar previousPartners={previousPartners} />
+      <button type="button">Add</button>
+      <button type="button">Cancel</button>
+      <button type="button">Save</button>
     </div>
   );
 }
