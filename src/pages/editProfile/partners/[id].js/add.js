2ef4452e8e6a7318fb83539e62c4cf6f97d2364c
DanteAguilarB <142114920+DanteAguilarB@users.noreply.github.com>
2024-11-02 13:48:57 -0400

    ahhhh

--- src/pages/editProfile/partners/[id].js/add.js
+++ src/pages/editProfile/partners/[id].js/add.js
@@ -1,22 +1,18 @@
-import ScrollBar from "../../../../components/ScrollBar";
-// import { useRouter } from "next/router";
+import ScrollBar from "../../../../../components/ScrollBar";
 
-export default function EditPartners() {
-  // const router = useRouter();
-
+export default function ProfileAddPartners() {
   const previousPartners = [
     { id: 1, name: "Previous Partner 1" },
     { id: 2, name: "Previous Partner 2" },
     { id: 3, name: "Previous Partner 3" },
   ];
 
-  // add complete function that works with data base
-
   return (
     <div>
       <h2>Edit Partners</h2>
       <ScrollBar previousPartners={previousPartners} />
-      <button type="button">Add</button>
+      <input />
+      <input />
       <button type="button">Cancel</button>
       <button type="button">Save</button>
     </div>
