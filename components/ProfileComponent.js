782698cd86e8a03aa40a6fc95025c1e4e9b7f1ba
DanteAguilarB <142114920+DanteAguilarB@users.noreply.github.com>
2024-11-04 18:01:43 -0500

    disabled linter checks

--- components/ProfileComponent.js
+++ components/ProfileComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "../src/styles/ProfileComponent.module.css";
