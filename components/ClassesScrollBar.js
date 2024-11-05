2d5ce90be4d182a63843521acc2e21da9edc369a
Andrew Kriter <akriter@middlebury.edu>
2024-11-04 19:24:51 -0500

    more stuff 6

--- components/ClassesScrollBar.js
+++ components/ClassesScrollBar.js
@@ -1,10 +1,8 @@
 import PropTypes from "prop-types";
 import styles from "../src/styles/Classes.module.css";
 import ClassShape from "./ClassShape";
-import { useState } from "react";
-import { useRouter } from "next/router";
 
-export default function ClassesScrollBar(classesTaken) {
+export default function ClassesScrollBar( {classesTaken} ) {
 
   classesTaken.forEach((element) => {
     console.log(element.name);
@@ -23,25 +21,7 @@ export default function ClassesScrollBar(classesTaken) {
   }
 
   return (
-  <div className={styles.classesContainer}>
-    <ul>
-      {classesTaken.map((classItem) => (
-        <li key={classItem.id}>{classItem.name}</li>
-      ))}
-    </ul>
-    
-    <input
-      type="text"
-      value={newClass}
-      onChange={(e) => setNewClass(e.target.value)}
-      placeholder="Enter class name"
-      className={styles.inputField}
-    />
-    
-    <button onClick={handleAdd} className={styles.addButton}>Add</button>
-    <button onClick={handleSave} className={styles.saveButton}>Save</button>
-    <button onClick={handleCancel} className={styles.saveButton}>Cancel</button>
-  </div>
+  <div className={styles.classesContainer}> </div>
   )
 }
   
--- src/pages/editProfile/classes/add.js
+++ src/pages/editProfile/classes/add.js
@@ -1,30 +1,20 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import ClassesScrollBar from "../../../../components/ClassesScrollBar";
-// import { useRouter } from "next/router";
 
-export default function EditPartners() {
-  // const router = useRouter();
-
+export default function ProfileAddPartners() {
   const classesTaken = [
     { id: 1, name: "CS201" },
     { id: 2, name: "CS202" },
     { id: 3, name: "CS318" },
   ];
 
-  // add complete function that works with data base
-
   return (
     <div>
-      <h2>Edit Partners</h2>
+      <h2>Add Classes</h2>
       <ClassesScrollBar classesTaken={classesTaken} />
-      <a href="/editProfile/classes/add">
-        <button type="button">Add</button>
-      </a>
-      <a href="/">
+      <input placeholder="Class name (ex: CS145)" />
+      <a href="/editProfile/classes">
         <button type="button">Cancel</button>
-      </a>
-      <a href="/">
-        <button type="button">Save</button>
       </a>
     </div>
   );
