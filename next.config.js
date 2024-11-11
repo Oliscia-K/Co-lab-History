87d4d41deccdf15cbd5ab366ba3db9b4138b34e8
DanteAguilarB <142114920+DanteAguilarB@users.noreply.github.com>
2024-11-11 13:16:12 -0500

    functional name search, dummy filter

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/homepage",
+        permanent: false,
+      },
+    ];
+  },
+  eslint: {
+    // Since we are using production build for test server, we choose to ignore linting errors during
+    // building. We can do so because we run the linter in other parts of our workflow.
+    ignoreDuringBuilds: true,
+  },
+};
