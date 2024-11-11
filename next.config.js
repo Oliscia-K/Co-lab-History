e2f61fed83d557182a97fb7749e892462d528f18
DanteAguilarB <142114920+DanteAguilarB@users.noreply.github.com>
2024-11-11 16:38:04 -0500

    fixed config file

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/homepage",
-        permanent: false,
-      },
-    ];
-  },
-  eslint: {
-    // Since we are using production build for test server, we choose to ignore linting errors during
-    // building. We can do so because we run the linter in other parts of our workflow.
-    ignoreDuringBuilds: true,
-  },
 };
+
+module.exports = nextConfig;
