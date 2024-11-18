d8acb44c6838b4e73201b4125ec9edc479df63dd
Evan Lin <elin@middlebury.edu>
2024-11-18 17:11:27 -0500

    fixed profile component and image uploader

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -6,22 +6,20 @@ function ImageUploader({ onImageUpload }) {
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      // create a BLOB (Binary Large Object)
-      const blob = new Blob([file], { type: file.type });
-      onImageUpload(blob);
+      const reader = new FileReader();
+
+      // define the onload event to handle the binary data after reading
+      reader.onloadend = () => {
+        // The result will be an ArrayBuffer (binary data)
+        const binaryData = reader.result;
+        // Pass the binary data to the parent component
+        onImageUpload(binaryData);
+      };
+
+      // Read the file as an ArrayBuffer instead of BLOB
+      reader.readAsArrayBuffer(file);
     }
   };
-
-  /*
-<<<<<<< HEAD
-  const uploadBlob = async (blob, filename) =>{
-    
-  }
-=======
-
-  const uploadArray = async () =>{
->>>>>>> 1c12e5d5e0ecb1d8a3fd8c0a6f0b4e416e90ddc4
-  */
 
   return (
     <div>
